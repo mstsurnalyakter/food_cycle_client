a89425823a1b53ec23676c61d66af055c2b8eb37
mstsurnalyakter <surnalyms@gmail.com>
2024-11-06 10:44:24 +0600

    update: Home Page

--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Adult from '../../components/Adult'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <div>
-        <Header/>
-        <div className='flex items-center justify-center mt-10'>
-            <button className='bg-primary text-white py-2 px-9 shadow-lg rounded-tl-lg rounded-bl-lg'>Adult</button>
-            <button className='bg-secondary py-2 px-9 text-primary shadow-lg rounded-tr-lg rounded-br-lg'>Child</button>
-        </div>
-        <Adult/>
+      <Header />
+      <div className="flex items-center justify-center mt-10">
+        <button className="bg-primary text-white py-2 px-9 shadow-lg rounded-tl-lg rounded-bl-lg">
+          Adult
+        </button>
+        <Link to="/child">
+          <button className="bg-secondary py-2 px-9 text-primary shadow-lg rounded-tr-lg rounded-br-lg">
+            Child
+          </button>
+        </Link>
+      </div>
+      <Adult />
     </div>
-  )
+  );
 }
 
 export default HomePage
